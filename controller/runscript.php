3fd6d76f9787273da9121b1fb3c25075e7815f7a
johnraypaulin14 <androidscamex@gmail.com>
2017-07-27 21:51:33 +0800

    Enable run script to run validation.

---
+++ controller/runscript.php
@@ -0,0 +1,49 @@
+<?php
+
+
+/*
+	echo 'UID' . $_POST['user_id'];
+	echo 'PID' . $_POST['project_id'];
+	echo 'F' . $_POST['file'];
+
+	var_dump($_FILES);
+	var_dump($_POST); */
+
+	// $filename = $_FILES["data_feed"]["tmp_name"];
+	// echo 'File Name '.$filename;
+
+if (isset($_POST['project_id']) AND isset($_POST['user_id'])){
+
+
+	$pid = $_POST['project_id'];
+	$uid = $_POST['user_id'];
+	$filename = $_POST['data_feed'];
+	// $filename = $_FILES["file"]["tmp_name"];	
+	// print_r($filename);
+
+	// var_dump($_POST);
+	// var_dump($_FILES);
+
+	if(sizeof($_FILES) == 2){
+		
+		$base_feed 		= $_FILES['data_feed']['tmp_name'];
+		$product_feed 	= $_FILES['product_feed']['tmp_name'];
+
+		echo $data_feed . ' ' . $product_feed;
+
+	echo system("php ". 'DataValidationController.php' . ' ' . $base_feed . ' ' . $product_feed . ' ' . $pid . ' ' . $uid);
+
+	}else{
+		echo "Operation aborted. There is only 1 file detected.";
+	}
+	// var_dump($_POST);
+	
+	
+	
+}else{
+	
+	die("No post data");
+}
+
+
+?>
