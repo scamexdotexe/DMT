9e94d15f062ea37d89398305b6c914acdb2edc61
johnraypaulin14 <androidscamex@gmail.com>
2017-07-27 21:51:16 +0800

    Updated data validation.

--- controller/DataValidationController.php
+++ controller/DataValidationController.php
@@ -1,56 +1,91 @@
 <?php
-include_once("../datafeed-db_config.php");
-global $con_datafeed_db;
+error_reporting(0);
 
-if (isset($_GET['user_id'])) {
-	$user_id = $_GET['user_id'];
-}else{
-	$user_id = $_POST['user_id'];
-}
-if (isset($_GET['project_id'])) {
-	$project_id = $_GET['project_id'];
-}else{
-	$project_id = $_POST['project_id'];
-}
+$basefeed 			= urldecode($argv[1]);
+$productfeed 		= urldecode($argv[2]);
+$pid 				= urldecode($argv[3]);
+$uid 				= urldecode($argv[4]);
 
- $file = $_FILES['data_feed']['tmp_name'];
- 
- echo 'FILE: '.$file;
- $handle = fopen($file, "r");
- $counter = 0;
- while(($filesop = fgetcsv($handle, 1000, "\t")) !== false)
-	{
-		echo 'counter: '.$counter;
-		if($counter != 0){
-			//capturing all the columns and data and separate it into Variable
-			$product_type = $filesop[0];
-			$title = $filesop[1];
-			$description = $filesop[2];
-			$link = $filesop[3];
-			$image_link = $filesop[4];
-			$id = $filesop[5];
-			$label = $filesop[6];
-			$price = $filesop[7];
-			$size = $filesop[8];
-			$availability = $filesop[9];
-			$condition = $filesop[10];
-			$brand = $filesop[11];
-			$google_product_category = $filesop[12];
-			$gender = $filesop[13];
-			$age_group = $filesop[14];
-			$mpn = $filesop[15];
-			$color = $filesop[16];
-			$item_group_id = $filesop[17];
-			$adwords_labels = $filesop[18];
-			$style_number = $filesop[19];
-			$GTIN = $filesop[20];
+$productFeed 	= file_get_contents($productfeed);
+$dataFeed		= file_get_contents($basefeed);
 
-			//echo $counter. ' product_type: '.$product_type;
-			echo '<br/>';echo '<br/>';
-			echo $counter. ' title: '.$title;
-			echo '<br/>';echo '<br/>';
-			
-			//-----------------------------------------Product Type Validation
+$productLine 	= explode("\n",$productFeed);
+$productCols	= explode("\t",$productFeed); // array with all columns
+
+echo "[+] Products Length: ".count($productCols)."\n";
+echo "[+] Products Lines Length: ".count($productLine)."\n";
+
+$dataFeedLines  = explode("\n",$dataFeed);
+$dataFeedCols   = explode("\t",$productFeed);
+
+$newDataFeed	= "product_type\ttitle\tdescription\tlink\timage_link\tid\tlabel\tprice\tsize\tavailability\tcondition\tbrand\tgoogle_product_category\tgender\tage_group\tmpn\tcolor\titem_group_id\tstyle_number\tGTIN\n";
+$removedData	= "product_type\ttitle\tdescription\tlink\timage_link\tid\tlabel\tprice\tsize\tavailability\tcondition\tbrand\tgoogle_product_category\tgender\tage_group\tmpn\tcolor\titem_group_id\tstyle_number\tGTIN\n";
+
+echo "[+] Data Length: ".count($dataFeedLines)."\n";
+
+// if not in productfeed then remove from datafeed
+// update status in datafeed with status from productfeed
+
+$i = 0;
+foreach ($dataFeedLines as $key => $value) {
+	$i++;
+	if ($i == 1){
+		continue;
+	}
+	
+	//product_type	title	description	link	image_link	id	label	price	size	availability	condition	brand	google_product_category	gender	age_group	mpn	color	item_group_id	style_number	GTIN
+	//each lines again splitted to objects by comma
+	
+	$each_line 					= explode("\t", $value);	
+	$product_type 				= str_replace('\"','',$each_line[0]);
+	$title 						= str_replace('\"','',$each_line[1]);
+	$description 				= str_replace('\"','',$each_line[2]);
+	$link 						= str_replace('\"','',$each_line[3]);
+	$image_link 				= str_replace('\"','',$each_line[4]);
+	$id 						= str_replace('\"','',$each_line[5]);
+	$label 						= str_replace('\"','',$each_line[6]);
+	$price 						= str_replace('\"','',$each_line[7]);
+	$size 						= str_replace('\"','',$each_line[8]);
+	$availability 				= str_replace('\"','',$each_line[9]);
+	$item_condition 			= str_replace('\"','',$each_line[10]);
+	$brand		    			= str_replace('\"','',$each_line[11]);
+	$google_product_category 	= str_replace('\"','',$each_line[12]);
+	$gender 					= str_replace('\"','',$each_line[13]);
+	$age_group 					= str_replace('\"','',$each_line[14]);
+	$mpn 						= str_replace('\"','',$each_line[15]);
+	$color 						= str_replace('\"','',$each_line[16]);
+	$item_group_id				= str_replace('\"','',$each_line[17]);
+	$style_number 				= str_replace('\"','',$each_line[18]);
+	$gtin		 				= preg_replace('/\s+/', '', $each_line[19]);
+
+	/*
+	if in datafeed but not ptoduct feed = remove
+
+	if in product feed but not datafeed = add
+
+	if in both files adjust in stock, price fields
+	*/
+	$key = array_search($id, $productCols); //Searches the array for a given value and returns the first corresponding key if successful
+	if ($key !== false) {
+		$newStatus = $productCols[$key+4];
+		if ($newStatus == "Y"){
+			$newAvailability = "in stock";
+		}elseif ($newStatus == "N"){
+			$newAvailability = "out of stock";
+		}else{
+			$newAvailability = "";
+		}
+	}
+
+	if ((in_array($link,$productCols)) AND (in_array($id,$productCols))){
+		// if in datafeed and in productfeed ==> adjust in stock, price fields
+		$newDataFeed .= "$product_type \t $title \t $description \t $link \t $image_link \t $id \t $label \t $price \t $size \t $newAvailability \t $item_condition \t $brand \t $google_product_category \t $gender \t $age_group \t $mpn \t $color \t $item_group_id \t $style_number \t $gtin \n";
+	}else{
+		// if in datafeed but not in productfeed ==> remove
+		$removedData .= "$product_type \t $title \t $description \t $link \t $image_link \t $id \t $label \t $price \t $size \t $availability \t $item_condition \t $brand \t $google_product_category \t $gender \t $age_group \t $mpn \t $color \t $item_group_id \t $style_number \t $gtin \n";
+	}
+
+				//-----------------------------------------Product Type Validation
 				//echo 'product_type: '. $product_type;
 				if($product_type == '' OR $product_type == null){
 					//failed 
@@ -67,7 +102,9 @@ if (isset($_GET['project_id'])) {
 				}else{
 					//success
 				}
-				
+
+
+
 			//-----------------------------------------Title Validation
 				//echo 'title: '. $title;
 				if($title == '' OR $title == null){
@@ -246,12 +283,12 @@ if (isset($_GET['project_id'])) {
 			}
 		 
 		//-----------------------------------------CONDITION Validation
-			echo 'condition: '. $condition;
-			if($condition == '' OR $condition == null){
+			echo 'condition: '. $item_condition;
+			if($item_condition == '' OR $item_condition == null){
 				//failed
 				$warning_field = 'condition';
 				$warning_message = 'no data';
-				$other_data = $condition;
+				$other_data = $item_condition;
 				insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data);
 			}
 		 
@@ -363,18 +400,9 @@ if (isset($_GET['project_id'])) {
 				insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data);
 			}
 			
-		//-----------------------------------------adwords_labels Validation
-			echo 'adwords_labels: '. $adwords_labels; 
-			if($adwords_labels == '' OR $adwords_labels == null){
-				//failed
-				$warning_field = 'adwords_labels';
-				$warning_message = 'no data';
-				$other_data = $adwords_labels;
-				insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data);
-			}
-			
+					
 		//-----------------------------------------style_number Validation
-			echo 'style_number: '. $style_number;
+			
 			if($style_number == '' OR $style_number == null){
 				//failed
 				$warning_field = 'style_number';
@@ -384,36 +412,84 @@ if (isset($_GET['project_id'])) {
 			}
 		 
 		//-----------------------------------------GTIN Validation
-			echo 'GTIN: '. $GTIN;
-			if($GTIN == '' OR $GTIN == null){
+			
+			if($gtin == '' OR $gtin == null){
 				//failed
 				$warning_field = 'GTIN';
 				$warning_message = 'no data';
-				$other_data = $GTIN;
+				$other_data = $gtin;
 				insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data);
-			}elseif(strlen($GTIN) > 50){
+			}elseif(strlen($gtin) > 50){
 				//failed
 				$warning_field = 'GTIN';
 				$warning_message = 'Max 50 numeric characters ';
-				$other_data = $GTIN;
+				$other_data = $gtin;
 				insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data);
 			}
-		}	 
-		$counter++;
+
+
+
+	echo ".";
+
+
+}
+
+
+foreach ($productLine as $key => $value) {
+	$i++;
+	if ($i == 1){
+		continue;
 	}
 	
+	//product_type	title	description	link	image_link	id	label	price	size	availability	condition	brand	google_product_category	gender	age_group	mpn	color	item_group_id	style_number	GTIN
+	//each lines again splitted to objects by comma
+	$each_line 					= explode("\t", $value);	
+	$product_type 				= str_replace('\"','',$each_line[0]);
+	$title 						= str_replace('\"','',$each_line[1]);
+	$description 				= str_replace('\"','',$each_line[2]);
+	$link 						= str_replace('\"','',$each_line[3]);
+	$image_link 				= str_replace('\"','',$each_line[4]);
+	$id 						= str_replace('\"','',$each_line[5]);
+	$label 						= str_replace('\"','',$each_line[6]);
+	$price 						= str_replace('\"','',$each_line[7]);
+	$size 						= str_replace('\"','',$each_line[8]);
+	$availability 				= str_replace('\"','',$each_line[9]);
+	$item_condition 			= str_replace('\"','',$each_line[10]);
+	$brand		    			= str_replace('\"','',$each_line[11]);
+	$google_product_category 	= str_replace('\"','',$each_line[12]);
+	$gender 					= str_replace('\"','',$each_line[13]);
+	$age_group 					= str_replace('\"','',$each_line[14]);
+	$mpn 						= str_replace('\"','',$each_line[15]);
+	$color 						= str_replace('\"','',$each_line[16]);
+	$item_group_id				= str_replace('\"','',$each_line[17]);
+	$style_number 				= str_replace('\"','',$each_line[18]);
+	$gtin		 				= preg_replace('/\s+/', '', $each_line[19]);
+	
+	$key = array_search($id, $dataFeedCols);
+	if ($key !== false) {
+		// if in product feed but not datafeed = add
+		$removedData .= "$product_type \t $title \t $description \t $link \t $image_link \t $id \t $label \t $price \t $size \t $availability \t $item_condition \t $brand \t $google_product_category \t $gender \t $age_group \t $mpn \t $color \t $item_group_id \t $style_number \t $gtin \n";
+	}
+}
+
+// Write the contents back to the file
+file_put_contents("new_datafeedversion4.txt", $newDataFeed);
+file_put_contents("removedData.txt", $removedData);
+
+echo "Done. Files created.";
+
+
+
 function insertWarning($con_datafeed_db,$warning_field,$warning_message,$other_data){
 	echo 'warning field: '.$warning_field;
 	global $con_datafeed_db,$warning_field,$warning_message,$other_data,$user_id,$project_id;
 	$stmtdatafeed = $con_datafeed_db->prepare("INSERT INTO warning_list (project_id,user_id,warning_field,warning_message,other_data) VALUES (?,?,?,?,?)");
 	$stmtdatafeed->execute(array('1',$user_id,$warning_field,$warning_message,$other_data));
 	//exit(header('Location: ' . $_SERVER['HTTP_REFERER']));
-	redirect($user_id);
+	// redirect($user_id);
 }
 
-function redirect($user_id){
-	?>
-		<script>window.location = "../view/datafeed.php?id=<?php echo $user_id?>";</script>
-	<?php
-}
+
+
+
 ?>
